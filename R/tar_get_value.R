85795886d9cffed7baf3354d89712d44632ec516
TarJae <you@example.com>
2024-05-26 08:46:03 +0200

    updated tar_get_value function (can take now more elements, instead of only one)

--- R/tar_get_value.R
+++ R/tar_get_value.R
@@ -1,7 +1,11 @@
 # Function to handle unquoted inputs
 #' Look-up function
-
-#' @param mykeyvalue an unquoted key or value
+#' @description This function takes a data frame df and a vector of
+#' key values mykeyvalues. It returns an unnamed vector of corresponding
+#' results by performing lookups in the data frame.
+#' @param mykeyvalues is a vector containing the key values to be looked up in the data frame.
+#' These key values can be either numeric or character strings. The function processes each key
+#' value individually to determine if it matches entries in the specified columns of the data frame
 #' @param df data frame with Name and azl number
 #' @importFrom stringr str_detect
 #' @importFrom stringr str_to_lower
@@ -13,34 +17,37 @@
 #' # Example data
 #' df <- data.frame(Name = c("Hans", "Maria", "Franz HUBER"), azl = c("1006341612", "1040405318", "1060707219"), stringsAsFactors = FALSE)
 #'
-#' tar_get_value(df, HUBER)       # Should return "1060707219"
-#' tar_get_value(df, huber)       # Should also return "1060707219"
-#' tar_get_value(df, Franz)       # Should return "1060707219"
+#' tar_get_value(df, "HUBER")       # Should return "1060707219"
+#' tar_get_value(df, "huber")       # Should also return "1060707219"
+#' tar_get_value(df, "Franz")       # Should return "1060707219"
 #' tar_get_value(df, 1006341612)  # Should return "Hans"
+#' tar_get_value(df, c(1006341612, 1040405318)) # Should return "Hans"  "Maria"
 
 
 # Function to handle inputs for data frame
-tar_get_value <- function(df, mykeyvalue) {
-  # Capture the unquoted input and convert to character
-  mykeyvalue_char <- as.character(substitute(mykeyvalue))
+tar_get_value <- function(df, mykeyvalues) {
+  results <- c()
 
-  # Check if the input is numeric or character
-  if (grepl("^[0-9]+$", mykeyvalue_char)) {
-    # Input is numeric (azl)
-    if (mykeyvalue_char %in% df$azl) {
-      return(df$Name[df$azl == mykeyvalue_char])
+  for (mykeyvalue in mykeyvalues) {
+    if (is.numeric(mykeyvalue)) {
+      mykeyvalue <- as.character(mykeyvalue)
+      if (mykeyvalue %in% df$azl) {
+        results <- c(results, df$Name[df$azl == mykeyvalue])
+      } else {
+        stop("Numeric key not found in data frame")
+      }
+    } else if (is.character(mykeyvalue)) {
+      matches <- str_detect(str_to_lower(df$Name), fixed(str_to_lower(mykeyvalue)))
+      if (any(matches)) {
+        results <- c(results, df$azl[matches])
+      } else {
+        stop("Name key not found in data frame")
+      }
     } else {
-      stop("Numeric key not found in data frame")
-    }
-  } else {
-    # Input is non-numeric (Name)
-    # Use case-insensitive and partial matching
-    matches <- str_detect(str_to_lower(df$Name), fixed(str_to_lower(mykeyvalue_char)))
-    if (any(matches)) {
-      return(df$azl[matches])
-    } else {
-      stop("Name key not found in data frame")
+      stop("Key must be either numeric or character")
     }
   }
+
+  return(results)
 }
 
